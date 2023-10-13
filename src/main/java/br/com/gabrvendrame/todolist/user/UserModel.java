0f50e03e23791589c0215a28605171878afa3b6a
GabrVendrame <gabrielvendrame7@gmail.com>
2023-10-13 10:02:04 -0300

    Aula 02

--- src/main/java/br/com/gabrvendrame/todolist/user/UserModel.java
+++ src/main/java/br/com/gabrvendrame/todolist/user/UserModel.java
@@ -1,7 +1,28 @@
 package br.com.gabrvendrame.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
+
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    
+    @Column(unique = true)
     private String username;
     private String name;
     private String password;
+
+    private LocalDateTime createdAt;
+
 }
